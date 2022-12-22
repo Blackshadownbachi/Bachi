022ddc3aac335427fb070394aa440ab740ef49a0
Blackshadownbachi <117249911+Blackshadownbachi@users.noreply.github.com>
2022-12-22 22:29:16 +0530

    Update Bachi.sh

--- Bachi.sh
+++ Bachi.sh
@@ -1,14 +1,21 @@
-clear echo " 𝐉𝐨𝐢𝐧  my 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 Account  "
 
- echo "" 
+clear
+echo " 𝐉𝐨𝐢𝐧  my 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 Account"|lolcat
+ echo ""
+ echo " Blackshadow"|figlet |lolcat
+ echo " "
+ echo " ☠️ 🄲🅈🄱🄴🅁 🅆🄰🅁🄽🄸🄽🄶 ⚠️ "| lolcat
+ echo " "
+ echo " this tool developer : "|lolcat
+ echo "Bachi"|figlet|lolcat
+echo""
+ echo "____________________"
+ sleep 1
+ while :
 
- echo " Blackshadowl" 
+ do
 
- echo " " 
+xdg-open https://bachianonymous.w3spaces.com/
+am start --user 0 -n
+com.android.chrome/com.google.android.apps.chrome.Main
 
- echo " ☠️ 🄲🅈🄱🄴🅁 🅆🄰🅁🄽🄸🄽🄶 ⚠️ " echo " " 
-
- echo " this tool developer : Black shadow " sleep 2 
-
-xdg-open Blackshadowl while : do xdg-open https://bachianonymous.w3spaces.com/ am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main 
-done 
