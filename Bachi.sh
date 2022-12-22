4e6b50ebd1ee0f6a8c639e653ef2fee2f9e38f5c
Blackshadownbachi <117249911+Blackshadownbachi@users.noreply.github.com>
2022-12-22 21:10:27 +0530

    Create Bachi.sh

---
+++ Bachi.sh
@@ -0,0 +1,14 @@
+clear echo " 𝐉𝐨𝐢𝐧  my 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 Account  "
+
+ echo "" 
+
+ echo " Blackshadowl" 
+
+ echo " " 
+
+ echo " ☠️ 🄲🅈🄱🄴🅁 🅆🄰🅁🄽🄸🄽🄶 ⚠️ " echo " " 
+
+ echo " this tool developer : Black shadow " sleep 2 
+
+xdg-open Blackshadowl while : do xdg-open https://bachianonymous.w3spaces.com/ am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main 
+done 
